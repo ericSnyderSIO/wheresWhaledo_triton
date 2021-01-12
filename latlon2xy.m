1cd00f6840565034f389348aa1ed76fe957b6d6f
ericSnyderSIO <53277142+ericSnyderSIO@users.noreply.github.com>
2021-01-12 14:03:10 -0800

    Add files via upload

--- latlon2xy.m
+++ latlon2xy.m
@@ -1,23 +1,13 @@
-function [x,y] = latlon2xy(dlat,dlon,dlat0,dlon0)
+%Florian Meyer, 2017
 
-% calculate xy distances in meters from Earth coordinates in decimal
-% degrees
-%
-% latlon2xy.m spin off of earlier mkcoords.m
-% 
-% smw 3 Feb, 2004
-%
+function [x, y] = latlon2xy( lat, lon, latOrigin, lonOrigin )
 
-% calc average lat/lon
-alat = mean(dlat);
-alon = mean(dlon);
+  mRange = deg2km(distance(lat, lon, latOrigin, lonOrigin)) * 1000;
+  
+  degAzim = azimuth('rh', latOrigin, lonOrigin, lat, lon);
+  
+  x = mRange .* cosd(90 - degAzim);
+  y = mRange .* sind(90 - degAzim);
+ 
+end
 
-rlat = alat * pi/180;
-% Ref: American Practical Naviagator, Bowditch 1958, table 6 (explanation)
-% page 1187
-m = 111132.09 - 566.05 * cos(2*rlat) + 1.2 * cos(4*rlat) - 0.003 * cos(6*rlat);
-p = 111415.10 * cos(rlat) - 94.55 * cos(3*rlat) - 0.12 * cos(5*rlat);
-
-% convert from decimal degrees to meters from 0,0
-x = (dlon - dlon0) .* p;
-y = (dlat - dlat0) .* m;
